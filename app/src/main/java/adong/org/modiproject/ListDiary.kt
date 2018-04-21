fee77392ff5a06b1cca25a67d5db555c843726ac
adongbyh <byou0070@gmail.com>
2018-04-22 08:09:11 +0900

    Third commit(UI 변경)

---
+++ app/src/main/java/adong/org/modiproject/ListDiary.kt
@@ -0,0 +1,19 @@
+package adong.org.modiproject
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ListDiary : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_listdiary, container, false)
+    }
+
+
+}
--- app/src/main/java/adong/org/modiproject/MainActivity.kt
+++ app/src/main/java/adong/org/modiproject/MainActivity.kt
@@ -1,13 +1,48 @@
 package adong.org.modiproject
 
+import adong.org.modiproject.adapter.TabAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
+import android.widget.TableLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tabs : TabLayout
+    lateinit var viewPager : ViewPager
+    lateinit var adapter : TabAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabs = findViewById(R.id.tabs)
+        tabs.addTab(tabs.newTab().setIcon(R.drawable.list))
+        tabs.addTab(tabs.newTab().setIcon(R.drawable.pencil))
+        tabs.addTab(tabs.newTab().setIcon(R.drawable.question))
+        tabs.tabGravity = TabLayout.GRAVITY_CENTER
+
+        adapter = TabAdapter(supportFragmentManager)
+        viewPager = findViewById(R.id.viewpager)
+        viewPager.adapter = adapter
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+
+        tabs.setOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                viewPager.currentItem = tab!!.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+
 
     }
 }
