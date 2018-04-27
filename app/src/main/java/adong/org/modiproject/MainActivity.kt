f27f77b837ba6777876e482ab0ec48f25f8a2889
adongbyh <byou0070@gmail.com>
2018-04-28 00:06:49 +0900

    sixth commit(main 화면 구축)

--- app/src/main/java/adong/org/modiproject/MainActivity.kt
+++ app/src/main/java/adong/org/modiproject/MainActivity.kt
@@ -1,14 +1,49 @@
 package adong.org.modiproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
+    lateinit var fab : FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.maintoolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(resources.getColor(R.color.white))
 
+        fab = findViewById(R.id.fab)
+        fab.setOnClickListener({
+            startActivity(Intent(applicationContext, WriteActivity::class.java))
+        })
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item!!.itemId) {
+            R.id.action_logout -> {
+                Toast.makeText(applicationContext, "로그아웃", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_abote -> {
+                Toast.makeText(applicationContext, "도움말", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
